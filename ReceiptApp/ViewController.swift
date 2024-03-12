dc0132ae942d8b706e6535c4265f91a8f3bc59c3
Bruno Alves <alves.bruno945@gmail.com>
2024-03-11 22:00:44 -0300

    Extract files to own folder

--- ReceiptApp/ViewController.swift
+++ ReceiptApp/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  ReceiptApp
-//
-//  Created by Bruno Silva on 11/03/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
-
