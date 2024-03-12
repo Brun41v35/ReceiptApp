9ea8b22e121a9e6f99c24afe41e352858ef3a751
Bruno Alves <alves.bruno945@gmail.com>
2024-03-11 22:28:45 -0300

    Add `ReceiptView` and `ReceiptViewControlller`

---
+++ ReceiptApp/Scenes/Receipt/View/ReceiptView.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+final class ReceiptView: UIView {
+
+    // MARK: - Private Properties
+
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.showsVerticalScrollIndicator = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .red
+        return tableView
+    }()
+
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Private Methods
+
+    private func setup() {
+        addHierarchyView()
+        setupConstraints()
+        setupBackgroundColor()
+    }
+
+    private func addHierarchyView() {
+        addSubview(tableView)
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8)
+        ])
+    }
+
+    private func setupBackgroundColor() {
+        backgroundColor = .systemBackground
+    }
+}
