c2eb388ec0534886aa5a7f63bcf654e295a0c4c7
Bruno Alves <alves.bruno945@gmail.com>
2024-03-13 22:08:31 -0300

    Making the `ReceiptListViewController ` `UITableViewDelegate` and `UITableViewDataSource`

--- ReceiptApp/Scenes/Receipt/ReceiptListViewController.swift
+++ ReceiptApp/Scenes/Receipt/ReceiptListViewController.swift
@@ -27,5 +27,37 @@ final class ReceiptListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableViewDataSource()
     }
+
+    // MARK: - Setup
+
+    private func setupTableViewDataSource() {
+        contentView.show(dataSource: self, delegate: self)
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension ReceiptListViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ReceiptListViewCell", for: indexPath) as? ReceiptListViewCell else {
+            return UITableViewCell()
+        }
+        cell.show(viewModel: ReceiptListViewModelCell(title: "title", name: "name", date: "date", amount: "amount"))
+
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension ReceiptListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
 }
