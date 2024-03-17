871678045ab3ba2b2f2e00667b0bae9dbae580df
Bruno Alves <alves.bruno945@gmail.com>
2024-03-17 16:35:29 -0300

    Update `NetworkManagerMock`

--- ReceiptApp/Commons/Network/NetworkManagerMock.swift
+++ ReceiptApp/Commons/Network/NetworkManagerMock.swift
@@ -2,5 +2,18 @@ import Foundation
 
 final class NetworkManagerMock: NetworkManagerType {
 
-    func fetchData(completion: @escaping (Result<ReceiptData, APIError>) -> Void) {}
+    func fetchData(completion: @escaping (Result<ReceiptData, APIError>) -> Void) {
+
+        guard let path = Bundle.main.path(forResource: "receiptData", ofType: "json") else { return }
+        let url = URL(filePath: path)
+
+        do {
+            let decoder = JSONDecoder()
+            let data = try Data(contentsOf: url, options: .mappedIfSafe)
+            let response = try decoder.decode(ReceiptData.self, from: data)
+            completion(.success(response))
+        } catch {
+            completion(.failure(.invalidData))
+        }
+    }
 }
